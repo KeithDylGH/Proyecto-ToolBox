715f24c0603b20947213ebdc9c309123255d4d33
KeithDylGH <KeithDylTM@gmail.com>
2024-06-12 18:06:13 -0400

    ToolBox-DevAlpha2.3

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require('./app')
-const http = require('http')
-const server = http.createServer(app)
 
-server.listen(8000, ()=>{
-    console.log('Esta activo el server')
+const PORT = process.env.PORT || 8000
+
+app.listen(PORT, ()=>{
+    console.log(`Esta activo el server ${PORT}`);
 })
