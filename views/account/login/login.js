9f2bb644bf12b5c72660f9c6860d95a470769b33
KeithDylGH <KeithDylTM@gmail.com>
2024-06-13 21:07:03 -0400

    ToolBox-Ver1.8

---
+++ views/account/login/login.js
@@ -0,0 +1,19 @@
+//Entrar a la pagina Registrar
+document.addEventListener('DOMContentLoaded', function() {
+
+    const botonCrearCuenta = document.getElementById('create');
+
+    botonCrearCuenta.addEventListener('click', function() {
+        window.location.href = '../register/index.html';
+    });
+});
+
+//Entrar a la pagina Home
+document.addEventListener('DOMContentLoaded', function() {
+
+    const botonACasa = document.getElementById('homeBtn');
+
+    botonACasa.addEventListener('click', function() {
+        window.location.href = '../home/index.html';
+    });
+});
