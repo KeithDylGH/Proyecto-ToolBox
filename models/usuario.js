2059a5ce4b973f2d722bdbe098b625830237a4d3
KeithDylGH <KeithDylTM@gmail.com>
2024-06-13 21:33:25 -0400

    ToolBox-Ver1.8.7

--- models/usuario.js
+++ models/usuario.js
@@ -3,16 +3,20 @@ const userRouter = require('../controllers/usuarios')
 
 //definir el esquema para el usuario
 const usuarioSchema = new mongoose.Schema({
-    nombre:String
-})
+    nombre: String,
+    correo: String,
+    password: String
+});
 
 //configurar la respuesta del usuario en el esquema
-usuarioSchema.set('toJSON',{
-    transform:(document,returnedObject)=>{
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
+usuarioSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString();
+        delete returnedObject._id;
+        delete returnedObject.__v; // Eliminamos la versión de control de la base de datos
+        delete returnedObject.password; // No devolvemos la contraseña por seguridad
     }
-})
+});
 
 //registrar el modelo
 const user = mongoose.model('user',usuarioSchema)
--- views/account/login/login.js
+++ views/account/login/login.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const botonCrearCuenta = document.getElementById('create');
 
     botonCrearCuenta.addEventListener('click', function() {
-        window.location.href = '../register/index.html';
+        window.location.href = '../views/register/index.html';
     });
 });
 
@@ -14,6 +14,6 @@ document.addEventListener('DOMContentLoaded', function() {
     const botonACasa = document.getElementById('homeBtn');
 
     botonACasa.addEventListener('click', function() {
-        window.location.href = '../home/index.html';
+        window.location.href = '../views/home/index.html';
     });
 });
