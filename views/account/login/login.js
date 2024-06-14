88e4a3f910efa7a5fb4512e9c7852ed51098c97b
KeithDylGH <KeithDylTM@gmail.com>
2024-06-13 21:54:11 -0400

    ToolBox-Ver1.8.9DEV

--- views/account/login/login.js
+++ views/account/login/login.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const botonCrearCuenta = document.getElementById('create');
 
     botonCrearCuenta.addEventListener('click', function() {
-        window.location.href = '../views/register/index.html';
+        window.location.href = '../register/index.html';
     });
 });
 
@@ -14,6 +14,34 @@ document.addEventListener('DOMContentLoaded', function() {
     const botonACasa = document.getElementById('homeBtn');
 
     botonACasa.addEventListener('click', function() {
-        window.location.href = '../views/home/index.html';
+        window.location.href = '../home/index.html';
+    });
+});
+
+
+//formulario Login
+
+document.addEventListener('DOMContentLoaded', function() {
+    const loginForm = document.getElementById('loginForm');
+
+    loginForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        const usuario = document.getElementById('usuario').value;
+        const password = document.getElementById('password').value;
+
+        axios.post('http://localhost:4000/api/usuarios/login', { usuario, password })
+            .then(response => {
+                const { token } = response.data;
+                // Almacenar el token en localStorage o sessionStorage para su uso posterior
+                localStorage.setItem('token', token);
+                // Redirigir a la página principal o realizar otra acción
+                window.location.href = './index.html'; // Cambiar a la página que desees después del login
+            })
+            .catch(error => {
+                console.error('Error de inicio de sesión:', error.response.data.error);
+                // Mostrar mensaje de error al usuario, por ejemplo:
+                alert('Error de inicio de sesión. Verifica tus credenciales.');
+            });
     });
 });
