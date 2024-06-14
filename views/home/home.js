8bef2790c97aedd86fdc597804cd8860bf6cfd91
KeithDylGH <KeithDylTM@gmail.com>
2024-06-13 20:56:55 -0400

    ToolBox-Ver 1.7.3

--- views/home/home.js
+++ views/home/home.js
@@ -4,6 +4,6 @@ document.addEventListener('DOMContentLoaded', function() {
     const botonIniciarSesion = document.getElementById('login');
 
     botonIniciarSesion.addEventListener('click', function() {
-        window.location.href = '../account/login/';
+        window.location.href = '../login/index.html';
     });
 });
