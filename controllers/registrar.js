0638cccfe08e2c098da303973931bc8552d0882e
KeithDylGH <KeithDylTM@gmail.com>
2024-07-01 15:37:28 -0400

    ToolBox-Ver 1.19.2.4.1

--- controllers/registrar.js
+++ controllers/registrar.js
@@ -40,26 +40,3 @@ formulario.addEventListener('submit', async (e) => {
         alert(result.error);
     }
 });
-
-
-
-
-//Entrar a la pagina Home
-document.addEventListener('DOMContentLoaded', function() {
-
-    const homeBtn = document.getElementById('homeBtn');
-
-    homeBtn.addEventListener('click', function() {
-        window.location.href = '/';
-    });
-});
-
-//Entrar a la pagina Log-in
-document.addEventListener('DOMContentLoaded', function() {
-
-    const botonIniciarSesion = document.getElementById('login');
-
-    botonIniciarSesion.addEventListener('click', function() {
-        window.location.href = '/login/';
-    });
-});
