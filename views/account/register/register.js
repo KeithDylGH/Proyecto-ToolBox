5cb27f0439f0fe5282a3ace2782b2394a2c38e55
KeithDylGH <KeithDylTM@gmail.com>
2024-07-02 11:24:26 -0400

    ToolBox-Ver 1.20.4

--- views/account/register/register.js
+++ views/account/register/register.js
@@ -2,47 +2,51 @@
 
 const formulario = document.querySelector('#formulario');
 
-formulario.addEventListener('submit', async (e) => {
-    e.preventDefault();
+document.addEventListener('DOMContentLoaded', function(){
 
-    const nombre = document.getElementById('name').value;
-    const apellido = document.getElementById('lName').value;
-    const username = document.getElementById('username').value;
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-    const confirmPassword = document.getElementById('confirmPassword').value;
-    const phoneNumber = document.getElementById('phoneNumber').value;
-    const cedula = document.getElementById('cedula').value;
-
-    if (password !== confirmPassword) {
-        alert('Las contraseñas no coinciden');
-        return;
-    }
-
-    const response = await fetch('/api/users/registrar', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-            nombre: nombre,
-            apellido: apellido,
-            usuario: username,
-            correo: email,
-            password: password,
-            numero: phoneNumber,
-            cedula: cedula
-        }),
+    formulario.addEventListener('submit', async (e) => {
+        e.preventDefault();
+    
+        const nombre = document.getElementById('name').value;
+        const apellido = document.getElementById('lName').value;
+        const username = document.getElementById('username').value;
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('password').value;
+        const confirmPassword = document.getElementById('confirmPassword').value;
+        const phoneNumber = document.getElementById('phoneNumber').value;
+        const cedula = document.getElementById('cedula').value;
+    
+        if (password !== confirmPassword) {
+            alert('Las contraseñas no coinciden');
+            return;
+        }
+    
+        const response = await fetch('/api/users/registrar', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                nombre: nombre,
+                apellido: apellido,
+                usuario: username,
+                correo: email,
+                password: password,
+                numero: phoneNumber,
+                cedula: cedula
+            }),
+        });
+    
+        const result = await response.json();
+    
+        if (response.ok) {
+            alert(result.mensaje);
+            window.location.href = '/login/'; // Redirige al inicio de sesión
+        } else {
+            alert(result.error);
+        }
+        console.log('La respuesta del servidor: ', response)
     });
+})
 
-    const result = await response.json();
-
-    if (response.ok) {
-        alert(result.mensaje);
-        window.location.href = '/login/'; // Redirige al inicio de sesión
-    } else {
-        alert(result.error);
-    }
-    console.log('La respuesta del servidor: ', response)
-});
 
 
 
