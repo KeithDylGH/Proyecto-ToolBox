665d4ccba7cb563d200a1622544a03a88de73bad
KeithDylGH <KeithDylTM@gmail.com>
2024-07-02 17:46:21 -0400

    ToolBox-Ver1.21.5.4

--- views/account/cuenta/cliente/cuenta.js
+++ views/account/cuenta/cliente/cuenta.js
@@ -34,6 +34,6 @@ document.addEventListener('DOMContentLoaded', function() {
     const logout = document.getElementById('logout');
 
     logout.addEventListener('click', function() {
-        window.location.href = '/login/';
+        window.location.href = '/login';
     });
 });
