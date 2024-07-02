1a7cac160b48a9311f93d1d669ec747b2473c755
KeithDylGH <KeithDylTM@gmail.com>
2024-07-02 17:27:56 -0400

    ToolBox-Ver1.21.5.1

--- views/account/cuenta/cliente/cuenta.js
+++ views/account/cuenta/cliente/cuenta.js
@@ -1,35 +1,39 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const botonACasa = document.getElementById('homeBtn');
 
-    const sideBtn = document.getElementById('sideBtn');
-
-    sideBtn.addEventListener('click', function() {
+    botonACasa.addEventListener('click', function() {
         window.location.href = '/';
     });
 });
 
 document.addEventListener('DOMContentLoaded', function() {
-
     const carritoBtn = document.getElementById('carritoBtn');
 
     carritoBtn.addEventListener('click', function() {
-        window.location.href = '/';
+        window.location.href = '/cuenta/carrito';
     });
 });
 
 document.addEventListener('DOMContentLoaded', function() {
-
     const contactoBtn = document.getElementById('contactoBtn');
 
     contactoBtn.addEventListener('click', function() {
-        window.location.href = '/';
+        window.location.href = '/cuenta/contactos';
     });
 });
 
 document.addEventListener('DOMContentLoaded', function() {
-
     const configuracionBtn = document.getElementById('configuracionBtn');
 
     configuracionBtn.addEventListener('click', function() {
-        window.location.href = '/';
+        window.location.href = '/cuenta/configuracion';
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const logout = document.getElementById('logout');
+
+    logout.addEventListener('click', function() {
+        window.location.href = '/login/';
     });
 });
