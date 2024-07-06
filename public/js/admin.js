f9ea0c63b86ae9efe3943fb93aa51068c48edcf5
KeithDylGH <KeithDylTM@gmail.com>
2024-07-06 07:49:52 -0400

    ToolBox-Ver 1.23.6.1

--- public/js/admin.js
+++ public/js/admin.js
@@ -22,3 +22,5 @@ document.addEventListener('DOMContentLoaded', function(){
         window.location.href = '/inventario/VerProduto/'
     })
 })
+
+document.addEventListener('DOMContentLoaded', inventarioBtn)
