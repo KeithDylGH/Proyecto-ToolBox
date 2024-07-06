26064eda30d086249aa460e15a70f03e992b6ebe
KeithDylGH <KeithDylTM@gmail.com>
2024-07-06 13:23:40 -0400

    ToolBox-Ver 1.23.9.11.3

--- public/js/alerta.js
+++ public/js/alerta.js
@@ -1,17 +1,21 @@
-export function mostrarAlerta(mensaje){
+export function mostrarAlerta(mensaje) {
     const alerta = document.querySelector('.bg-red-100');
 
-    if(!alerta){
+    if (!alerta) {
+        const alertContainer = document.createElement('div');
+        alertContainer.classList.add('bg-red-100', 'border-red-400', 'text-red-700', 'px-4', 'py-3', 'rounded', 'text-center');
+        
         const alert = document.createElement('p');
-        alert.classList.add('bg-red-100','border-red-400','text-red-700','px-4','py-3','rounded','text-center');
         alert.innerHTML = `
             <strong>Error</strong>
             <span>${mensaje}</span>
-        `
-        alerta.appendChild(alert);
-
-        setTimeout(()=>{
-            alert.remove();
-        },2500)
+        `;
+        
+        alertContainer.appendChild(alert);
+        document.body.appendChild(alertContainer); // Agregar el contenedor al body o a algún otro elemento específico
+        
+        setTimeout(() => {
+            alertContainer.remove();
+        }, 2500);
     }
 }
