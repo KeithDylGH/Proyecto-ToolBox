35eb7146ede09bfd9c25b0a805a0827431ab96c8
KeithDylGH <KeithDylTM@gmail.com>
2024-07-06 12:42:24 -0400

    ToolBox-Ver 1.23.11

--- api.js
+++ api.js
@@ -2,7 +2,7 @@ const url = 'https://proyecto-toolbox.onrender.com/api/productos';
 
 export const nuevoProducto = async producto => {
     try {
-        await fetch(`${url}/agregar`, {
+        await fetch('/inventario/agregarproduto/', {
             method: 'POST',
             body: JSON.stringify(producto),
             headers: {
