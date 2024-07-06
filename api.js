e9f4f65fa7c3ab4bf87773959b07b8ba0c602667
KeithDylGH <KeithDylTM@gmail.com>
2024-07-06 10:29:48 -0400

    ToolBox-Ver 1.23.9.1

--- api.js
+++ api.js
@@ -1,18 +1,18 @@
-const url = 'http://localhost:3000/menu';
+const url = 'http://localhost:3000/api/productos';
 
 export const nuevoProducto = async producto => {
-    try{
-        await fetch(url,{
-            method:'POST',
-            body:JSON.stringify(producto),
-            headers:{
-                'Content-Type':'application/json'
+    try {
+        await fetch(`${url}/agregar`, {
+            method: 'POST',
+            body: JSON.stringify(producto),
+            headers: {
+                'Content-Type': 'application/json'
             }
-        })
-    }catch(error){
-        console.log(error)
+        });
+    } catch (error) {
+        console.log(error);
     }
-}
+};
 export const obtenerProductos = async () => {
     try{
         const resultado = await fetch(url);
