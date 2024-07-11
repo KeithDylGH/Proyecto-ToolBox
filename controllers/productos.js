9f9380bec67514dde16e01e2530b2a610ca78a02
KeithDylGH <KeithDylTM@gmail.com>
2024-07-10 23:00:17 -0400

    ToolBox-Ver 1.15.3.8.4

--- controllers/productos.js
+++ controllers/productos.js
@@ -2,6 +2,13 @@ const express = require('express');
 const router = express.Router();
 const producto = require('../models/producto');
 
+const {
+    obtenerProductos,
+    obtenerProducto,
+    editarProducto,
+    eliminarProducto
+} = require('./api');
+
 router.post('/admin/inventario', async (req, res) => {
     try {
         const nuevoProducto = new producto(req.body);
@@ -60,6 +67,43 @@ router.get('/verproducto', async (req, res) => {
     } catch (error) {
         console.error('Error al obtener los productos:', error);
         res.status(500).send('Error al obtener los productos');
+    }
+});
+
+// Ruta para mostrar el formulario de edición de un producto específico
+router.get('/inventario/editar/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const producto = await obtenerProducto(id);
+        res.render('editP', { producto });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error al obtener el producto para editar.');
+    }
+});
+
+// Ruta para guardar los cambios editados del producto
+router.post('/inventario/editar/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const { nombre, precio, categoria, descripcion } = req.body;
+        await editarProducto({ id, nombre, precio, categoria, descripcion });
+        res.redirect('/inventario'); // Redirigir a la página de ver productos después de editar
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error al guardar los cambios del producto.');
+    }
+});
+
+// Ruta para eliminar un producto
+router.delete('/inventario/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        await eliminarProducto(id);
+        res.sendStatus(200); // Envía estado 200 para indicar éxito
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error al eliminar el producto.');
     }
 });
 
