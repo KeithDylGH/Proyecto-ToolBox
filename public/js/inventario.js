7a6140ae61cce55cf753333cd6bb5acfc6406f4c
KeithDylGH <KeithDylTM@gmail.com>
2024-07-11 14:01:20 -0400

    ToolBox-Ver 1.25.3.9.1

--- public/js/inventario.js
+++ public/js/inventario.js
@@ -1,17 +1,25 @@
 import { eliminarProducto, editarProducto } from '/controllers/api';
 
-async function manejarEliminarProducto(productoId) {
-    if (confirm("¿Estás seguro de que quieres eliminar este producto?")) {
-        try {
-            await eliminarProducto(productoId);
-            alert('Producto eliminado con éxito');
-            location.reload(); // Recarga la página para reflejar los cambios
-        } catch (error) {
-            alert('Hubo un error al eliminar el producto');
-            console.error('Error:', error);
-        }
-    }
-}
+document.addEventListener('DOMContentLoaded', () => {
+    const botonesEliminar = document.querySelectorAll('.btn-eliminar');
+
+    botonesEliminar.forEach(boton => {
+        boton.addEventListener('click', async (event) => {
+            const productoId = event.target.id.replace('btn-eliminar-', '');
+
+            if (confirm("¿Estás seguro de que quieres eliminar este producto?")) {
+                try {
+                    await eliminarProducto(productoId);
+                    alert('Producto eliminado con éxito');
+                    location.reload(); // Recarga la página para reflejar los cambios
+                } catch (error) {
+                    alert('Hubo un error al eliminar el producto');
+                    console.error('Error:', error);
+                }
+            }
+        });
+    });
+});
 
 const manejarEditarProducto = async (id) => {
     try {
