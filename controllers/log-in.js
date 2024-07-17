3cf699b2aedb67b44748a8dfb3c8f0b27b303044
KeithDylGH <KeithDylTM@gmail.com>
2024-07-17 18:45:05 -0400

    ToolBox-Ver 1.26.3

--- controllers/log-in.js
+++ controllers/log-in.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
-const { buscarUsuarioPorNombre } = require('./buscarUsuario'); // Asegúrate de usar la función correcta
-
+const { buscarUsuarioPorNombre } = require('./buscarUsuario');
+const session = require('express-session');
 
 async function iniciarSesion(usuario, contraseña) {
     try {
-        const user = await buscarUsuarioPorNombre(usuario); // Usa la función correcta
+        const user = await buscarUsuarioPorNombre(usuario);
 
         if (!user) {
             console.log("No existe el usuario.");
@@ -32,10 +32,20 @@ router.post('/', async (req, res) => {
     const result = await iniciarSesion(usuario, contraseña);
 
     if (result.success) {
+        req.session.user = result.user; // Guardar usuario en la sesión
         res.json({ success: true, user: result.user });
     } else {
         res.status(400).json({ success: false, message: result.message });
     }
+});
+
+router.get('/logout', (req, res) => {
+    req.session.destroy((err) => {
+        if (err) {
+            return res.status(500).json({ success: false, message: 'Error al cerrar sesión' });
+        }
+        res.redirect('/');
+    });
 });
 
 module.exports = router;
