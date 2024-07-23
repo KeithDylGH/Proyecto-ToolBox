33dfc401e1f3b5b0a9588477e2804da17152f736
KeithDylGH <KeithDylTM@gmail.com>
2024-07-22 20:11:29 -0400

    ToolBox-Ver 1.26.4.14

--- controllers/log-in.js
+++ controllers/log-in.js
@@ -1,63 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
-const { buscarUsuarioPorNombre } = require('./buscarUsuario');
-const CUsuario = require('../models/usuario'); // Ajusta la ruta según tu estructura de directorios
+const { buscarUsuarioPorCorreo, buscarUsuarioPorNombre } = require('./buscarUsuario');
 const session = require('express-session');
-
-/* async function iniciarSesion(usuario, contraseña) {
-    try {
-        const user = await buscarUsuarioPorNombre(usuario);
-
-        if (!user) {
-            console.log("No existe el usuario.");
-            return { success: false, message: 'Usuario no encontrado' };
-        }
-
-        const passwordCorrecta = await bcrypt.compare(contraseña, user.password);
-        if (!passwordCorrecta) {
-            console.log("Contraseña incorrecta.");
-            return { success: false, message: 'Contraseña incorrecta' };
-        }
-
-        console.log("Usuario encontrado exitosamente.");
-        return { success: true, user };
-    } catch (error) {
-        console.error('Error al iniciar sesión:', error);
-        return { success: false, message: 'Error en el servidor' };
-    }
-} */
 
 // Iniciar sesión
 router.post('/', async (req, res) => {
     const { correo, password } = req.body;
-    const user = await CUsuario.findOne({ correo });
-
-    if (user && await bcrypt.compare(password, user.password)) {
-        req.session.user = {
-            username: user.nombre,
-            role: user.role
-        };
-        console.log('Sesión iniciada:', req.session.user); // Verificar en la consola del servidor
-        res.redirect('/'); // Redirige a la página principal o donde prefieras
-    } else {
-        res.redirect('/login'); // Redirige al login si el usuario no es válido
-    }
-});
-
-router.post('/login', async (req, res) => {
-    const { correo, password } = req.body;
     try {
-        const user = await CUsuario.findOne({ correo });
+        const user = await buscarUsuarioPorCorreo(correo);
 
         if (user && await bcrypt.compare(password, user.password)) {
             req.session.user = {
                 username: user.nombre,
                 role: user.rol
             };
-            console.log('Sesión iniciada:', req.session.user);
+            console.log('Sesión iniciada:', req.session.user); // Verificar en la consola del servidor
             res.redirect('/'); // Redirige a la página principal o donde prefieras
         } else {
+            console.log('Correo o contraseña incorrectos');
             res.redirect('/login'); // Redirige al login si el usuario no es válido
         }
     } catch (error) {
@@ -65,8 +26,6 @@ router.post('/login', async (req, res) => {
         res.redirect('/login');
     }
 });
-
-
 
 // Cerrar sesión
 router.get('/logout', (req, res) => {
