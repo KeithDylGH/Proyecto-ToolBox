b7cced51b2133328acfcadfb9041f51711c31540
KeithDylGH <KeithDylTM@gmail.com>
2024-07-22 20:04:05 -0400

    ToolBox-Ver 1.26.4.13

--- controllers/log-in.js
+++ controllers/log-in.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const { buscarUsuarioPorNombre } = require('./buscarUsuario');
+const CUsuario = require('../models/usuario'); // Ajusta la ruta según tu estructura de directorios
 const session = require('express-session');
 
-async function iniciarSesion(usuario, contraseña) {
+/* async function iniciarSesion(usuario, contraseña) {
     try {
         const user = await buscarUsuarioPorNombre(usuario);
 
@@ -25,7 +26,7 @@ async function iniciarSesion(usuario, contraseña) {
         console.error('Error al iniciar sesión:', error);
         return { success: false, message: 'Error en el servidor' };
     }
-}
+} */
 
 // Iniciar sesión
 router.post('/', async (req, res) => {
@@ -46,19 +47,25 @@ router.post('/', async (req, res) => {
 
 router.post('/login', async (req, res) => {
     const { correo, password } = req.body;
-    const user = await CUsuario.findOne({ correo });
+    try {
+        const user = await CUsuario.findOne({ correo });
 
-    if (user && await bcrypt.compare(password, user.password)) {
-        req.session.user = {
-            username: user.nombre,  // Guardar el nombre del usuario
-            role: user.rol // Guardar el rol del usuario
-        };
-        console.log('Sesión iniciada:', req.session.user); // Verificar en la consola del servidor
-        res.redirect('/'); // Redirige a la página principal o donde prefieras
-    } else {
-        res.redirect('/login'); // Redirige al login si el usuario no es válido
+        if (user && await bcrypt.compare(password, user.password)) {
+            req.session.user = {
+                username: user.nombre,
+                role: user.rol
+            };
+            console.log('Sesión iniciada:', req.session.user);
+            res.redirect('/'); // Redirige a la página principal o donde prefieras
+        } else {
+            res.redirect('/login'); // Redirige al login si el usuario no es válido
+        }
+    } catch (error) {
+        console.error('Error al iniciar sesión:', error);
+        res.redirect('/login');
     }
 });
+
 
 
 // Cerrar sesión
