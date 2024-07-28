306f4833644a85d3f433539b185e746c0693d8f9
KeithDylGH <KeithDylTM@gmail.com>
2024-07-28 14:12:14 -0400

    ToolBox-Ver 1.26.6.6.1 (Multer)

--- app.js
+++ app.js
@@ -110,7 +110,7 @@ app.use(session({
 
 // Configuración de archivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/uploads', express.static(path.join(__dirname, 'public/uploads')));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Configurar EJS
 app.set('view engine', 'ejs');
