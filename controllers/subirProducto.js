e0882bb044433e059377b191eda338f37c3e0018
KeithDylGH <KeithDylTM@gmail.com>
2024-07-28 20:18:47 -0400

    ToolBox-Ver 1.26.6.9.4

--- controllers/subirProducto.js
+++ controllers/subirProducto.js
@@ -1,21 +1,26 @@
 require('dotenv').config();
 const fetch = require('node-fetch'); // Asegúrate de instalar node-fetch si no lo has hecho
+const fs = require('fs');
+const path = require('path');
 
-async function subirImagen(file) {
-    const url = `https://${process.env.bunnyNetHOSTNAME}/${process.env.bunnyNetZONE}/${file.name}`;
+async function subirImagen(filePath) {
+    // Leer el archivo como Buffer
+    const fileBuffer = fs.readFileSync(filePath);
+    const fileName = path.basename(filePath);
+    const url = `https://storage.bunnycdn.com/${process.env.bunnyNetZONE}/${fileName}`;
     const apiKey = process.env.bunnyNetAPIKEY;
 
     const response = await fetch(url, {
         method: 'PUT',
         headers: {
-            'Content-Type': file.type,
+            'Content-Type': 'application/octet-stream', // Ajusta el Content-Type según sea necesario
             'AccessKey': apiKey
         },
-        body: file
+        body: fileBuffer
     });
 
     if (!response.ok) {
-        throw new Error('Error al subir la imagen');
+        throw new Error(`Error al subir la imagen: ${response.statusText}`);
     }
 
     return url;
