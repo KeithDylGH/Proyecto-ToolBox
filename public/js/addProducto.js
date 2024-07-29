5fe0dfe829c3c5df34dba9cd90c25831b5819a1d
KeithDylGH <KeithDylTM@gmail.com>
2024-07-28 21:17:40 -0400

    ToolBox-Ver 1.26.6.9.8.1

--- public/js/addProducto.js
+++ public/js/addProducto.js
@@ -33,7 +33,7 @@ async function validarProducto(e) {
             imagenUrl
         };
 
-        const response = await fetch('/api/productos/agregar', {
+        const response = await fetch('/api/products/agregar', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
