63b180fc307427e4d5e27c8e07e22bc0b94a398e
KeithDylGH <KeithDylTM@gmail.com>
2024-07-30 00:27:05 -0400

    ToolBox-Ver 1.26.9.6.5.22

--- models/producto.js
+++ models/producto.js
@@ -1,27 +1,14 @@
 const mongoose = require('mongoose');
 
 const productoSchema = new mongoose.Schema({
-    nombre: {
-        type: String,
-        required: true,
-    },
-    precio: {
-        type: Number,
-        required: true,
-    },
-    categoria: {
-        type: String,
-        required: true,
-    },
-    descripcion: {
-        type: String,
-        required: true,
-    },
-    imagen: {
-        type: String, // Cambiar a String para almacenar la URL de la imagen
-    },
+    nombre: { type: String, required: true },
+    precio: { type: Number, required: true },
+    categoria: { type: Number, required: true },
+    descripcion: { type: String, required: true },
+    imagen: { 
+        data: { type: String }, // URL de la imagen en Bunny Storage
+        contentType: { type: String } 
+    }
 });
 
-const Producto = mongoose.model('Producto', productoSchema);
-
-module.exports = Producto;
+module.exports = mongoose.model('Producto', productoSchema);
