c91b46fc5aa0dbc9220ec46a1072b631daed0d5e
KeithDylGH <KeithDylTM@gmail.com>
2024-08-06 18:37:36 -0400

    ToolBox-Ver 1.29.1.4.3

--- public/js/carroDeCompra.js
+++ public/js/carroDeCompra.js
@@ -1,16 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
     const notification = document.querySelector('.notification');
   
+    // Función para mostrar notificación
     function mostrarNotificacion(mensaje, tipo = 'success') {
       notification.textContent = mensaje;
       notification.classList.add('alert', `alert-${tipo}`);
+      notification.style.display = 'block'; // Asegurarse de que la notificación esté visible
   
       setTimeout(() => {
         notification.textContent = '';
         notification.classList.remove('alert', `alert-${tipo}`);
+        notification.style.display = 'none'; // Ocultar la notificación después de un tiempo
       }, 3000);
     }
   
+    // Agregar al carrito
     document.querySelectorAll('.agregar-carrito').forEach(button => {
       button.addEventListener('click', function(event) {
         event.preventDefault();
@@ -24,7 +28,7 @@ document.addEventListener('DOMContentLoaded', function() {
         .then(response => response.json())
         .then(data => {
           if (data.success) {
-            mostrarNotificacion(data.message);
+            mostrarNotificacion(data.message, 'success');
           } else {
             mostrarNotificacion(data.error, 'danger');
           }
