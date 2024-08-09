ce7a107821e6a7f600c172cd51a2ae59a5b57a52
KeithDylGH <KeithDylTM@gmail.com>
2024-08-08 21:20:13 -0400

    ToolBox-Ver 1.29.3.1.8

--- public/js/carroDeCompras.js
+++ public/js/carroDeCompras.js
@@ -1,4 +1,138 @@
-// Define la base URL de Bunny Storage
+const agregarAlCarrito = async (productoId) => {
+    try {
+        const response = await fetch('/carrito/add-to-cart', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ productoId })
+        });
+
+        const result = await response.json();
+        if (result.success) {
+            // Mostrar la estrella de like
+            const likeEffect = document.getElementById('likeEffect');
+            likeEffect.style.display = 'block'; // Mostrar el contenedor de la estrella
+
+            // Ocultar la estrella después de la animación
+            setTimeout(() => {
+                likeEffect.style.display = 'none';
+            }, 1000); // La duración de la animación en CSS es de 1 segundo
+
+            const producto = result.producto;
+            const item = document.createElement('li');
+            item.classList.add('list-group-item', 'd-flex', 'align-items-center');
+            item.dataset.productoId = producto._id;
+            item.innerHTML = `
+                <img src="${producto.imagen.data}" alt="${producto.nombre}" class="img-fluid me-3" style="width: 100px; height: auto;">
+                <div>
+                    <small class="font-weight-bold mb-2">${producto.nombre}</small>
+                    <span class="text-muted">${producto.categoria}</span>
+                    <button class="btn btn-danger btn-sm ms-2 btn-remove" data-producto-id="${producto._id}">Eliminar</button>
+                </div>
+            `;
+            carritoItems.appendChild(item);
+
+        } else {
+            showAlert(result.message);
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        showAlert('Error al añadir producto al carrito');
+    }
+};
+
+const eliminarDelCarrito = async (productoId) => {
+    try {
+        const response = await fetch('/carrito/remove-from-cart', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ productoId })
+        });
+
+        const result = await response.json();
+        if (result.success) {
+            const item = document.querySelector(`li[data-producto-id="${productoId}"]`);
+            if (item) {
+                item.remove();
+            }
+        } else {
+            showAlert(result.message);
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        showAlert('Error al eliminar producto del carrito');
+    }
+};
+
+const cargarCarrito = async () => {
+    try {
+        const response = await fetch('/carrito/get-cart', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        const result = await response.json();
+
+        if (result.success) {
+            carritoItems.innerHTML = '';
+
+            result.carrito.forEach(producto => {
+                const item = document.createElement('li');
+                item.classList.add('list-group-item', 'd-flex', 'align-items-center');
+                item.dataset.productoId = producto._id;
+
+                const imagenUrl = producto.imagen.data;
+
+                item.innerHTML = `
+                    <img src="${imagenUrl}" alt="${producto.nombre}" class="img-fluid me-3" style="width: 100px; height: auto;">
+                    <div>
+                        <h4 class="font-weight-bold mb-2">${producto.nombre}</h4>
+                        <span class="text-muted">${producto.categoria}</span>
+                        <button class="btn btn-danger btn-sm ms-2 btn-remove" data-producto-id="${producto._id}">Eliminar</button>
+                    </div>
+                `;
+
+                carritoItems.appendChild(item);
+            });
+
+            document.querySelectorAll('.btn-remove').forEach(button => {
+                button.addEventListener('click', async (event) => {
+                    event.preventDefault();
+                    const productoId = button.getAttribute('data-producto-id');
+                    await eliminarDelCarrito(productoId);
+                });
+            });
+
+        } else {
+            showAlert('Error al cargar carrito: ' + result.message);
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        showAlert('Error al cargar carrito');
+    }
+};
+
+document.getElementById('verCarrito').addEventListener('click', () => {
+    cargarCarrito();
+    const carritoModal = new bootstrap.Modal(document.getElementById('carritoModal'));
+    carritoModal.show();
+});
+
+document.querySelectorAll('.btn-agregar-carrito').forEach(button => {
+    button.addEventListener('click', function(event) {
+        event.preventDefault();
+        const productoId = this.getAttribute('data-producto-id');
+        agregarAlCarrito(productoId);
+    });
+});
+
+
+/* // Define la base URL de Bunny Storage
 const bunnyBaseUrl = 'https://toolboxProject.b-cdn.net'; // Reemplaza esto con tu URL base de Bunny Storage
 
 function getImageUrl(imagenPath) {
@@ -100,4 +234,4 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 // Cargar el carrito cuando se abre el modal
-document.getElementById('carritoModal').addEventListener('show.bs.modal', cargarCarrito);
+document.getElementById('carritoModal').addEventListener('show.bs.modal', cargarCarrito); */
