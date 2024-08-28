47ae0c40a9b6d04d6bb8f78ed582d9ec57ac2963
KeithDylGH <KeithDylTM@gmail.com>
2024-08-28 14:44:14 -0400

    ToolBox-Ver 1.29.3.2.1

--- public/js/home.js
+++ public/js/home.js
@@ -16,3 +16,41 @@ document.addEventListener('DOMContentLoaded', function() {
         window.location.href = '/tienda/';
     });
 });
+
+//CARRITO DE COMPRAS
+$(document).ready(function () {
+    // Evento de clic para el botón "Agregar al Carrito"
+    $('.btn-agregar-carrito').click(function () {
+        var productoId = $(this).data('producto-id');
+        $.ajax({
+            url: '/agregar-al-carrito',
+            method: 'POST',
+            data: { productoId: productoId },
+            success: function (response) {
+                mostrarNotificacion(response.mensaje);
+                actualizarCarrito(response.carrito);
+            },
+            error: function () {
+                mostrarNotificacion('Error al agregar el producto al carrito');
+            }
+        });
+    });
+
+    // Función para mostrar notificación
+    function mostrarNotificacion(mensaje) {
+        var notificacion = $('.notification');
+        notificacion.text(mensaje);
+        notificacion.fadeIn().delay(3000).fadeOut();
+    }
+
+    // Función para actualizar el contenido del carrito
+    function actualizarCarrito(carrito) {
+        var carritoItems = $('#carritoItems');
+        carritoItems.empty();
+        carrito.forEach(function (item) {
+            carritoItems.append(
+                '<div>' + item.nombre + ' - Cantidad: ' + item.cantidad + '</div>'
+            );
+        });
+    }
+});
