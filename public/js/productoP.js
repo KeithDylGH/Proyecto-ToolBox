d7e3c5e7a3481429a76eb2f1eeb1a3684bb3b41b
KeithDylGH <KeithDylTM@gmail.com>
2024-09-09 23:33:53 -0400

    ToolBox-Ver 1.29.4.7.5.2

--- public/js/productoP.js
+++ public/js/productoP.js
@@ -1,30 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const agregarCarritoBtn = document.getElementById('agregarCarritoBtn');
-
-  if (agregarCarritoBtn) {
-    agregarCarritoBtn.addEventListener('click', async function() {
-      const productoId = agregarCarritoBtn.getAttribute('data-producto-id'); // Obtener productoId del atributo data-producto-id
-      const cantidad = 1; // Puedes ajustar la cantidad si es necesario
-
-      try {
-        const response = await fetch('/carrito/agregar', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({ productoId, cantidad })
-        });
-
-        const result = await response.json();
-        if (response.ok) {
-          alert('Producto añadido al carrito');
-        } else {
-          alert('Error al añadir el producto al carrito: ' + result.message);
+    const agregarCarritoBtn = document.getElementById('agregarCarritoBtn');
+  
+    if (agregarCarritoBtn) {
+      agregarCarritoBtn.addEventListener('click', async function() {
+        const productoId = '<%= producto._id %>'; // Esta línea se eliminará de aquí
+        const cantidad = 1;
+  
+        try {
+          const response = await fetch('/carrito/agregar', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ productoId, cantidad })
+          });
+  
+          const result = await response.json();
+          if (response.ok) {
+            alert('Producto añadido al carrito');
+          } else {
+            alert('Error al añadir el producto al carrito');
+          }
+        } catch (error) {
+          console.error('Error:', error);
+          alert('Error al añadir el producto al carrito');
         }
-      } catch (error) {
-        console.error('Error:', error);
-        alert('Error al añadir el producto al carrito');
-      }
-    });
-  }
-}); 
+      });
+    }
+  });
+  
