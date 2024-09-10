31afab22809c1ddbc973662ea74b6cbaa1683b82
KeithDylGH <KeithDylTM@gmail.com>
2024-09-09 23:41:34 -0400

    ToolBox-Ver 1.29.4.7.5.2.4

--- public/js/carritoC.js
+++ public/js/carritoC.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', function() {
             const cantidad = 1;
 
             try {
-                const response = await fetch('/carrito/add', {
+                const response = await fetch('/api/carrito/add', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json'
