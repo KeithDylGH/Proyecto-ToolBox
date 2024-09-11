22fc42315d7c3bf11076678eec4f40c0c36e6844
KeithDylGH <KeithDylTM@gmail.com>
2024-09-10 22:28:47 -0400

    ToolBox-Ver 1.30.1.6.2

--- public/js/compra.js
+++ public/js/compra.js
@@ -2,29 +2,31 @@ document.addEventListener('DOMContentLoaded', function () {
     const siguienteBtn = document.getElementById('siguienteBtn');
     const cancelarBtn = document.getElementById('cancelarBtn');
     const metodosPago = document.getElementById('metodosPago');
-    const card = document.querySelector('.card-body');
-
-    siguienteBtn.addEventListener('click', function () {
-        metodosPago.classList.remove('d-none'); // Mostrar los métodos de pago
-        siguienteBtn.style.display = 'none';    // Ocultar el botón "Siguiente"
-        cancelarBtn.style.display = 'none';     // Ocultar el botón "Cancelar"
-    });
+    
+    if (siguienteBtn && cancelarBtn && metodosPago) {
+        siguienteBtn.addEventListener('click', function () {
+            metodosPago.classList.remove('d-none');
+            siguienteBtn.style.display = 'none';
+            cancelarBtn.style.display = 'none';
+        });
+    }
 
     const pagoMovilBtn = document.getElementById('pagoMovilBtn');
     const paypalBtn = document.getElementById('paypalBtn');
     const cancelarPagoBtn = document.getElementById('cancelarPagoBtn');
 
-    pagoMovilBtn.addEventListener('click', function () {
-        const pagoMovilModal = new bootstrap.Modal(document.getElementById('pagoMovilModal'));
-        pagoMovilModal.show();
-    });
+    if (pagoMovilBtn && paypalBtn && cancelarPagoBtn) {
+        pagoMovilBtn.addEventListener('click', function () {
+            const pagoMovilModal = new bootstrap.Modal(document.getElementById('pagoMovilModal'));
+            pagoMovilModal.show();
+        });
 
-    paypalBtn.addEventListener('click', function () {
-        // Redirigir al usuario a la página de PayPal o a la API de PayPal
-        window.location.href = '/compra/paypal'; // Cambia esta URL según la configuración de tu API de PayPal
-    });
+        paypalBtn.addEventListener('click', function () {
+            window.location.href = '/compra/paypal';
+        });
 
-    cancelarPagoBtn.addEventListener('click', function () {
-        window.location.href = '/'; // Redirige al usuario a la página principal o a otra página de cancelación
-    });
+        cancelarPagoBtn.addEventListener('click', function () {
+            window.location.href = '/';
+        });
+    }
 });
