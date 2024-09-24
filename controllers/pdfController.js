123211bb53c8dc0e63fd1400fcd88ca9497a53b6
KeithDylGH <KeithDylTM@gmail.com>
2024-09-23 21:53:43 -0400

    ToolBox-Ver 1.30.5.4 (compraCarrito v3.8)

--- controllers/pdfController.js
+++ controllers/pdfController.js
@@ -12,6 +12,25 @@ exports.generarPdfCarrito = async (productosArray, metodo) => {
     const page = pdfDoc.addPage([600, 400]);
     const { width, height } = page.getSize();
 
+    // Cargar el logo
+    const logoPath = path.join(__dirname, '/img/logo/LogoLetra.png'); // Asegúrate de usar la ruta correcta
+    const logoBytes = fs.readFileSync(logoPath);
+    const logoImage = await pdfDoc.embedPng(logoBytes);
+    const logoDims = logoImage.scale(0.5); // Escalar el logo si es necesario
+
+    // Calcular la posición para centrar el logo
+    const logoX = (width - logoDims.width) / 2;
+    const logoY = height - logoDims.height - 10; // Espacio de 10px desde la parte superior
+
+    // Dibujar el logo en el PDF
+    page.drawImage(logoImage, {
+        x: logoX,
+        y: logoY,
+        width: logoDims.width,
+        height: logoDims.height,
+    });
+
+    // Agregar el título
     page.drawText('Factura de Compra', {
         x: 50,
         y: height - 50,
