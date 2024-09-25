0717f670c2c0c1f1df11f9b412d0261488fda466
KeithDylGH <KeithDylTM@gmail.com>
2024-09-24 20:29:21 -0400

    ToolBox-Ver 1.30.6.4 (PAYPAL V3.1) (EDITUSER V3.1) (CUENTA V2.2)

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -192,12 +192,13 @@ userRouter.put('/actualizar', async (req, res) => {
 // Endpoint para obtener los datos de un usuario por ID
 userRouter.get('/obtener/:id', async (req, res) => {
     const userId = req.params.id;
-    
+
     try {
         const user = await User.findById(userId);
         if (!user) {
             return res.status(404).json({ error: 'Usuario no encontrado' });
         }
+        // Aquí asegúrate de pasar 'usuario' a la vista
         res.render('editUser', { usuario: user });
     } catch (error) {
         console.error('Error al obtener el usuario:', error);
