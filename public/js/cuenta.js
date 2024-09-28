3e5f1a95e7e2b57225c81ffd2c6114cce0a54e27
KeithDylGH <KeithDylTM@gmail.com>
2024-09-28 19:55:55 -0400

    ToolBox-Ver 1.31.5.3.1

--- public/js/cuenta.js
+++ public/js/cuenta.js
@@ -41,13 +41,3 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 });
-
-
-router.get('/logout', (req, res) => {
-    req.session.destroy((err) => {
-        if (err) {
-            return res.status(500).json({ success: false, message: 'Error al cerrar sesión' });
-        }
-        res.redirect('/');
-    });
-});
