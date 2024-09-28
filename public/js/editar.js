fb13a85a7014538e0c738dd207b25bccc7d1f9e8
KeithDylGH <KeithDylTM@gmail.com>
2024-09-28 19:48:04 -0400

    ToolBox-Ver 1.31.5.3

--- public/js/editar.js
+++ public/js/editar.js
@@ -4,28 +4,46 @@ document.addEventListener('DOMContentLoaded', () => {
 
     form.addEventListener('submit', async (event) => {
         event.preventDefault();
-        
+
         const formData = new FormData(form);
         const id = form.getAttribute('action').split('/').pop();
+
+        // Mostrar el loader
+        document.querySelector('.loader-overlay').style.display = 'flex';
 
         try {
             const response = await fetch(`/api/products/editar/${id}`, {
                 method: 'PUT',
                 body: formData
-            });            
+            });
+
+            // Ocultar el loader
+            document.querySelector('.loader-overlay').style.display = 'none';
 
             if (response.ok) {
-                console.log('Producto actualizado con éxito');
-                window.location.href = '/inventario/verproducto';
+                // Mostrar notificación de éxito
+                showNotification('Producto añadido correctamente.', 'success');
+                // Redirigir a la página después de un pequeño delay para mostrar la notificación
+                setTimeout(() => {
+                    window.location.href = '/inventario/verproducto';
+                }, 2000);
             } else {
                 console.error('Error al actualizar el producto', await response.text());
+                showNotification('Error al actualizar el producto.', 'error');
             }
         } catch (error) {
+            // Ocultar el loader
+            document.querySelector('.loader-overlay').style.display = 'none';
             console.error('Error en el frontend:', error);
+            showNotification('Error en el frontend.', 'error');
         }
     });
 
     cancelBtn.addEventListener('click', () => {
-        window.location.href = '/inventario/verproducto';
+        // Mostrar el loader al cancelar
+        document.querySelector('.loader-overlay').style.display = 'flex';
+        setTimeout(() => {
+            window.location.href = '/inventario/verproducto';
+        }, 500); // Puedes ajustar el tiempo de espera si es necesario
     });
 });
