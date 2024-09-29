e9fd65f554432813293f1eb1b73d3e55953dc1b1
KeithDylGH <KeithDylTM@gmail.com>
2024-09-29 01:25:21 -0400

    ToolBox-Ver 1.31.6.4

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,59 +1,13 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const inventarioBtn = document.getElementById('inventarioBtn');
-    if (inventarioBtn) {
-        inventarioBtn.addEventListener('click', function() {
-            window.location.href = '/admin/inventario/';
-        });
-    }
+document.addEventListener("DOMContentLoaded", function() {
+    const formulario = document.getElementById("formulario");
 
-    const paginaAdmin = document.getElementById('admin');
-    if (paginaAdmin) {
-        paginaAdmin.addEventListener('click', function() {
-            window.location.href = '/admin/';
-        });
-    }
+    formulario.addEventListener("submit", function(event) {
+        // Mostrar el loader principal
+        document.querySelector('.loader-overlay').style.display = 'flex';
 
-    //INVENTARIO
-    const irALaPaginaDeInv = document.getElementById('invPag');
-
-    if (irALaPaginaDeInv) {
-        irALaPaginaDeInv.addEventListener('click', function() {
-            window.location.href = '/admin/inventario/';
-        });
-    }
-
-    const addPro = document.getElementById('addBtn');
-
-    if (addPro){
-        addPro.addEventListener('click', function() {
-            window.location.href = '/inventario/agregarproduto/'
-        })
-    }
-
-    const verProducto = document.getElementById('verBtn');
-
-    if (verProducto){
-        verProducto.addEventListener('click', function() {
-            window.location.href = '/inventario/verproducto/'
-        })
-    }
-
-    const verCategoria = document.getElementById('btnCat');
-
-    if (verCategoria){
-        verCategoria.addEventListener('click', function() {
-            window.location.href = '/inventario/categoria/'
-        })
-    }
-
-    const PDFyExcel = document.getElementById('dBtn');
-
-    if (PDFyExcel){
-        PDFyExcel.addEventListener('click', function() {
-            window.location.href = '/inventario/descargarInv/'
-        })
-    }
-
+        // Mostrar el mini-loader
+        showMiniLoader();
+    });
 });
 
 // Funciones para manejar el mini loader
